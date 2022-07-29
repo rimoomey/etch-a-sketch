fc9de2261fdd19d66570444db6c971ba79eab735
Rimon Moomey <rimon.moomey@gmail.com>
2022-07-29 13:56:20 -0500

    Create initial flexbox grid.

---
+++ scripts.js
@@ -0,0 +1,23 @@
+//add grid to html
+const gridContainer = document.getElementById('grid_container');
+console.log(gridContainer.getAttribute('id'));
+
+for(let i = 0; i < 4; i++) {
+
+    const gridCol = document.createElement('div');
+    gridCol.className ='grid-col';
+    gridContainer.appendChild(gridCol);
+}
+
+let num = 1;
+for(child of gridContainer.childNodes){
+
+    for(let i = 0; i < 4; i++) {
+
+        const gridBlock = document.createElement('div');
+        gridBlock.className ='grid-block';
+        gridBlock.textContent = num;
+        child.appendChild(gridBlock);
+        num++;
+    }
+}
