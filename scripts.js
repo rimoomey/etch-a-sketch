3db28d2d61e7cc3bfafe01e2ca2010ffd3cbac03
Rimon Moomey <rimon.moomey@gmail.com>
2022-07-29 14:37:17 -0500

    Make grid resizable via a button and user prompt.

--- scripts.js
+++ scripts.js
@@ -1,33 +1,70 @@
-//add grid to html
+//grid size constant
+let gridSize = 64;
 const gridContainer = document.getElementById('grid_container');
-console.log(gridContainer.getAttribute('id'));
 
-for(let i = 0; i < 4; i++) {
+//add grid to html
+function createGrid(gridSize){
+    
+    console.log(gridContainer.getAttribute('id'));
 
-    const gridCol = document.createElement('div');
-    gridCol.className ='grid-col';
-    gridContainer.appendChild(gridCol);
-}
+    for(let i = 0; i < gridSize; i++) {
 
-let num = 1;
-for(child of gridContainer.childNodes){
-
-    for(let i = 0; i < 4; i++) {
-
-        const gridBlock = document.createElement('div');
-        gridBlock.className ='grid-block';
-        gridBlock.textContent = num;
-        child.appendChild(gridBlock);
-        num++;
+        const gridCol = document.createElement('div');
+        gridCol.className ='grid-col';
+        gridContainer.appendChild(gridCol);
     }
+
+    //create children divs for columns
+    let num = 1;
+    for(child of gridContainer.childNodes){
+
+        for(let i = 0; i < gridSize; i++) {
+
+            const gridBlock = document.createElement('div');
+            gridBlock.className ='grid-block';
+            child.appendChild(gridBlock);
+            num++;
+        }
+    }
+
+    const blocks = document.querySelectorAll('.grid-block');
+
+    blocks.forEach((block) => block.addEventListener('mouseover', () => 
+                        changeBackground(block)));
+
 }
 
-const blocks = document.querySelectorAll('.grid-block');
+//run createGrid with default size
+createGrid(gridSize);
 
+//prompt user for grid size
+function userPrompt() {
+
+   return prompt("How big should the grid be?");
+}
+
+//change background color of block
 function changeBackground(el) {
 
     el.style.backgroundColor = 'black';
 }
 
-blocks.forEach((block) => block.addEventListener('mouseover', () => 
-                        changeBackground(block)));
+//get rid of present children between program runs
+function removeAllChildNodes(parent) {
+
+    while(parent.firstChild) {
+
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+//add button functionality
+const goButton = document.querySelector('button');
+
+//each click resets grid and allows user to create another
+goButton.addEventListener('click', () => {
+
+    removeAllChildNodes(gridContainer);
+    gridSize = userPrompt();
+    createGrid(gridSize);
+})
